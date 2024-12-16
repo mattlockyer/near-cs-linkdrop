dbe9bb83b6b7edbc00f88d960b7c87c23bf33400
mattlockyer <mattdlockyer@gmail.com>
2024-12-16 08:37:10 -0800

    contract and test updates

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -5,9 +5,8 @@ use near_sdk::{
     json_types::U128,
     log, near, require,
     store::{IterableMap, LookupMap},
-    Allowance, Gas, NearToken, PanicOnDefault, Promise, PromiseError, PublicKey,
+    AccountId, Allowance, Gas, NearToken, PanicOnDefault, Promise, PublicKey,
 };
-use serde_json::{from_str, Value};
 mod bitcoin_tx;
 mod ecdsa;
 mod external;
@@ -18,33 +17,38 @@ const ATTACHED_DEPOSIT: NearToken = NearToken::from_yoctonear(500000000000000000
 pub const ACCESS_KEY_METHODS: &str = "claim";
 pub const ACCESS_KEY_ALLOWANCE: NearToken = NearToken::from_millinear(10);
 
-#[derive(BorshSerialize, BorshDeserialize)]
+#[near(serializers = [json, borsh])]
+#[derive(Clone)]
 pub struct Drop {
     target: u8,
     amount: u128,
     funder: String,
+    keys: Vec<String>,
 }
 
 #[near(contract_state)]
 #[derive(PanicOnDefault)]
 pub struct Contract {
+    pub owner_id: AccountId,
     pub drop_id: u128,
-    pub drop_by_id: LookupMap<u128, Drop>,
-    pub drop_by_key: IterableMap<PublicKey, u128>,
+    pub drop_by_id: IterableMap<u128, Drop>,
+    pub drop_by_key: LookupMap<String, u128>,
 }
 
 #[near]
 impl Contract {
     #[init]
-    pub fn new() -> Self {
+    pub fn init(owner_id: AccountId) -> Self {
         Self {
+            owner_id,
             drop_id: 0,
-            drop_by_id: LookupMap::new(b"a"),
-            drop_by_key: IterableMap::new(b"b"),
+            drop_by_id: IterableMap::new(b"a"),
+            drop_by_key: LookupMap::new(b"b"),
         }
     }
 
     pub fn add_drop(&mut self, target: u8, amount: U128, funder: String) {
+        require!(env::predecessor_account_id() == self.owner_id);
         self.drop_id += 1;
         self.drop_by_id.insert(
             self.drop_id,
@@ -52,23 +56,33 @@ impl Contract {
                 target,
                 amount: amount.0,
                 funder,
+                keys: vec![],
             },
         );
     }
 
-    pub fn add_drop_key(&mut self, key: PublicKey, drop_id: U128) {
+    pub fn add_drop_key(&mut self, drop_id: U128, key: String) {
+        require!(env::predecessor_account_id() == self.owner_id);
         self.drop_by_key.insert(key.clone(), drop_id.0);
-        Promise::new(env::current_account_id()).add_access_key_allowance(
-            key,
-            Allowance::limited(ACCESS_KEY_ALLOWANCE).unwrap(),
-            env::current_account_id(),
-            ACCESS_KEY_METHODS.to_string(),
-        );
+
+        let mut drop = self.drop_by_id.get(&drop_id.0).unwrap().to_owned();
+        drop.keys.push(key.clone());
+        self.drop_by_id.insert(drop_id.0, drop);
+
+        let pk: PublicKey = key.parse().unwrap();
+        Promise::new(env::current_account_id())
+            .delete_key(pk.clone())
+            .add_access_key_allowance(
+                pk,
+                Allowance::limited(ACCESS_KEY_ALLOWANCE).unwrap(),
+                env::current_account_id(),
+                ACCESS_KEY_METHODS.to_string(),
+            );
     }
 
     pub fn claim(
         &mut self,
-        key: PublicKey,
+        key: String,
         txid_str: String,
         vout: u32,
         receiver: String,
@@ -90,73 +104,84 @@ impl Contract {
         // delete access key after successful callback
     }
 
-    pub fn test_claim(
-        &mut self,
-        txid_str: String,
-        vout: u32,
-        receiver: String,
-        funder: String,
-        amount: U128,
-        change: U128,
-    ) -> Promise {
-        let payload =
-            bitcoin_tx::get_tx_hash(&txid_str, vout, &funder, &receiver, amount.0, change.0);
+    // views
 
-        let path = "drop_path,1";
-        let key_version = 0;
-
-        ecdsa::get_sig(payload, path.to_owned(), key_version).then(
-            external::this_contract::ext(env::current_account_id())
-                .with_static_gas(CALLBACK_GAS)
-                .with_attached_deposit(ATTACHED_DEPOSIT)
-                .callback(txid_str, vout, receiver, funder, amount, change),
-        )
+    pub fn get_drops(&self) -> Vec<U128> {
+        self.drop_by_id.keys().map(|k| U128(*k)).collect()
     }
 
-    pub fn callback(
-        &mut self,
-        #[callback_result] call_result: Result<String, PromiseError>,
-        txid_str: String,
-        vout: u32,
-        receiver: String,
-        funder: String,
-        amount: U128,
-        change: U128,
-    ) -> String {
-        if call_result.is_err() {
-            log!("There was an error calling MPC Contract");
-            return "".to_string();
-        }
-
-        // Get the return value from the callback as a string
-        let success_value_str: String = call_result.unwrap();
-
-        let inner: serde_json::Value = serde_json::from_str(&success_value_str).unwrap();
-
-        let big_r = inner["big_r"]["affine_point"]
-            .as_str()
-            .ok_or("Missing big_r affine_point")
-            .unwrap()
-            .as_bytes();
-        let s = inner["s"]["scalar"]
-            .as_str()
-            .ok_or("Missing s scalar")
-            .unwrap()
-            .as_bytes();
-
-        // sig_vec will be 65 bytes in length
-        let mut sig_vec = vec![];
-        sig_vec.extend_from_slice(&mut big_r.to_vec());
-        sig_vec.extend_from_slice(&mut s.to_vec());
-
-        let omni_signature =
-            OmniSignature::SECP256K1(Secp256K1Signature(utils::vec_to_fixed(sig_vec)));
-
-        let omni_tx = bitcoin_tx::get_tx(&txid_str, vout, &funder, &receiver, amount, change);
-
-        let signed_omni_tx = omni_tx.build_with_signature(omni_signature);
-
-        // return signed raw tx as hex
-        encode(signed_omni_tx)
+    pub fn get_keys(&self, drop_id: U128) -> Vec<String> {
+        let drop = self.drop_by_id.get(&drop_id.0).unwrap();
+        drop.keys.clone()
     }
+
+    // pub fn test_claim(
+    //     &mut self,
+    //     txid_str: String,
+    //     vout: u32,
+    //     receiver: String,
+    //     funder: String,
+    //     amount: U128,
+    //     change: U128,
+    // ) -> Promise {
+    //     let payload =
+    //         bitcoin_tx::get_tx_hash(&txid_str, vout, &funder, &receiver, amount.0, change.0);
+
+    //     let path = "drop_path,1";
+    //     let key_version = 0;
+
+    //     ecdsa::get_sig(payload, path.to_owned(), key_version).then(
+    //         external::this_contract::ext(env::current_account_id())
+    //             .with_static_gas(CALLBACK_GAS)
+    //             .with_attached_deposit(ATTACHED_DEPOSIT)
+    //             .callback(txid_str, vout, receiver, funder, amount, change),
+    //     )
+    // }
+
+    // pub fn callback(
+    //     &mut self,
+    //     #[callback_result] call_result: Result<String, PromiseError>,
+    //     txid_str: String,
+    //     vout: u32,
+    //     receiver: String,
+    //     funder: String,
+    //     amount: U128,
+    //     change: U128,
+    // ) -> String {
+    //     if call_result.is_err() {
+    //         log!("There was an error calling MPC Contract");
+    //         return "".to_string();
+    //     }
+
+    //     // Get the return value from the callback as a string
+    //     let success_value_str: String = call_result.unwrap();
+
+    //     let inner: serde_json::Value = serde_json::from_str(&success_value_str).unwrap();
+
+    //     let big_r = inner["big_r"]["affine_point"]
+    //         .as_str()
+    //         .ok_or("Missing big_r affine_point")
+    //         .unwrap()
+    //         .as_bytes();
+    //     let s = inner["s"]["scalar"]
+    //         .as_str()
+    //         .ok_or("Missing s scalar")
+    //         .unwrap()
+    //         .as_bytes();
+
+    //     // sig_vec will be 65 bytes in length
+    //     let mut sig_vec = vec![];
+    //     sig_vec.extend_from_slice(&mut big_r.to_vec());
+    //     sig_vec.extend_from_slice(&mut s.to_vec());
+
+    //     let omni_signature =
+    //         OmniSignature::SECP256K1(Secp256K1Signature(utils::vec_to_fixed(sig_vec)));
+
+    //     let omni_tx = bitcoin_tx::get_tx(&txid_str, vout, &funder, &receiver, amount, change);
+
+    //     let signed_omni_tx = omni_tx.build_with_signature(omni_signature);
+
+    //     // return signed raw tx as hex
+    //     encode(signed_omni_tx)
+    // }
 }
--- tests/test.js
+++ tests/test.js
@@ -11,6 +11,9 @@ const { REACT_APP_accountId: accountId, REACT_APP_secretKey: secretKey } =
     process.env;
 const networkId = 'testnet';
 const keyPair = KeyPair.fromString(secretKey);
+const accessKeyPair = KeyPair.fromString(
+    'ed25519:5Da461pSxbSX8pc8L2SiQMwgHJJBYEovMVp7XgZRZLVbf1sk8pu139ie89MftYEQBJtN5dLc349FPXgUyBBE1mp1',
+);
 const keyStore = new keyStores.InMemoryKeyStore();
 keyStore.setKey(networkId, accountId, keyPair);
 const config = {
@@ -22,10 +25,11 @@ const config = {
     explorerUrl: 'https://testnet.nearblocks.io',
 };
 const near = new Near(config);
+const { connection } = near;
 
 // config for drop contract
 
-const contractId = 'drop.magical-part.testnet';
+const contractId = 'drop1.magical-part.testnet';
 keyStore.setKey(networkId, contractId, keyPair);
 
 // tests
@@ -33,7 +37,7 @@ keyStore.setKey(networkId, contractId, keyPair);
 test('account and balance test', async (t) => {
     const account = new Account(near.connection, accountId);
     const balance = await account.getAccountBalance();
-    console.log(balance);
+    // console.log(balance);
     t.pass();
 });
 
@@ -57,21 +61,155 @@ test('delete, create and deploy contract', async (t) => {
     }
 
     try {
-        const file = fs.readFileSync('./contract/out/contract.wasm');
+        const file = fs.readFileSync('./contract/target/near/contract.wasm');
         const account = new Account(near.connection, contractId);
         await account.deployContract(file);
+        console.log('deployed bytes', file.byteLength);
+        const balance = await account.getAccountBalance();
+        console.log('contract balance', balance);
     } catch (e) {
         console.log('error deployContract', e);
     }
 
-    console.log('account created');
-
     t.pass();
 });
 
-test('call contract', async (t) => {
-    const account = new Account(near.connection, accountId);
-    const balance = await account.getAccountBalance();
-    console.log(balance);
+test('init contract', async (t) => {
+    const res = await contractCall({
+        contractId,
+        methodName: 'init',
+        args: {
+            owner_id: accountId,
+        },
+    });
+    console.log(res);
     t.pass();
 });
+
+test('add drop', async (t) => {
+    const res = await contractCall({
+        contractId,
+        methodName: 'add_drop',
+        args: {
+            target: 1,
+            amount: '100000000', // 1 doge
+            funder: 'nkMesrm1adEvqDMzPBf1cko3hYstpFr4BM',
+        },
+    });
+    console.log(res);
+    t.pass();
+});
+
+test('add drop key', async (t) => {
+    const res = await contractCall({
+        contractId,
+        methodName: 'add_drop_key',
+        args: {
+            drop_id: '1',
+            key: accessKeyPair.getPublicKey().toString(),
+        },
+    });
+    console.log(res);
+    t.pass();
+});
+
+test('view drops', async (t) => {
+    const res = await contractView({
+        contractId,
+        methodName: 'get_drops',
+        args: {},
+    });
+    console.log(res);
+    t.pass();
+});
+
+test('view keys', async (t) => {
+    const res = await contractView({
+        contractId,
+        methodName: 'get_keys',
+        args: {
+            drop_id: '1',
+        },
+    });
+    console.log(res);
+    t.pass();
+});
+
+// helpers
+
+const gas = BigInt('300000000000000');
+const getAccount = (id = accountId) => new Account(connection, id);
+
+const contractView = async ({ contractId, methodName, args = {} }) => {
+    const account = getAccount();
+    let res;
+    try {
+        res = await account.viewFunction({
+            contractId,
+            methodName,
+            args,
+            gas,
+        });
+    } catch (e) {
+        if (/deserialize/gi.test(JSON.stringify(e))) {
+            console.log(`Bad arguments to ${methodName} method`);
+        }
+        throw e;
+    }
+    return res;
+};
+
+const contractCall = async ({ contractId, methodName, args }) => {
+    const account = getAccount();
+    let res;
+    try {
+        res = await account.functionCall({
+            contractId,
+            methodName,
+            args,
+            gas,
+        });
+    } catch (e) {
+        if (/deserialize/gi.test(JSON.stringify(e))) {
+            return console.log(`Bad arguments to ${methodName} method`);
+        }
+        if (e.context?.transactionHash) {
+            console.log(
+                `Transaction timeout for hash ${e.context.transactionHash} will attempt to get tx result in 20s`,
+            );
+            await sleep(getTxTimeout);
+            return getTxSuccessValue(e.context.transactionHash);
+        }
+        throw e;
+    }
+
+    return parseSuccessValue(res);
+};
+
+const getTxResult = async (txHash) => {
+    const transaction = await provider.txStatus(txHash, 'unnused');
+    return transaction;
+};
+
+const getTxSuccessValue = async (txHash) => {
+    const transaction = await getTxResult(txHash);
+    return parseSuccessValue(transaction);
+};
+
+const parseSuccessValue = (transaction) => {
+    if (transaction.status.SuccessValue.length === 0) return;
+
+    try {
+        return JSON.parse(
+            Buffer.from(transaction.status.SuccessValue, 'base64').toString(
+                'ascii',
+            ),
+        );
+    } catch (e) {
+        console.log(
+            `Error parsing success value for transaction ${JSON.stringify(
+                transaction,
+            )}`,
+        );
+    }
+};
